336c010039e27ee50d6c8dc695ce31102b3763dd
AndreyVShamin <addsharming@mail.ru>
2020-07-22 21:34:43 +0300

    first

---
+++ backup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+gbak -b -v -user SYSDBA -pass masterke /var/Motiw/db/motiw.fdb /home/motiw/backup/motiwboe.fdb
+
+
+
---
+++ dopftp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=`date +%s`
+echo $DIR
+echo $MOTIW
+lftp -e 'mirror --parallel=10 -R /home/motiw /motiw/'$DIR'/home' -u backup,$MOTIW 10.32.236.125
+lftp -e 'mirror --parallel=10 -R /var/Motiw /motiw/'$DIR'/var/Motiw' -u backup,$MOTIW 10.32.236.125
+lftp -e 'mirror --parallel=10 -R /etc /motiw/'$DIR'/etc' -u backup,$MOTIW 10.32.236.125
+
