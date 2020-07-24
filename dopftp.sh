208142fa3ec4ed8d3e438fc701f6f5d0030db46b
AndreyVShamin <addsharming@mail.ru>
2020-07-24 15:08:47 +0300

    Don't echo password

--- dopftp.sh
+++ dopftp.sh
@@ -2,7 +2,7 @@
 
 DIR=`date +%s`
 echo $DIR
-echo $MOTIW
+#echo $MOTIW
 lftp -e 'mirror --parallel=10 -R /home/motiw /motiw/'$DIR'/home' -u backup,$MOTIW 10.32.236.125
 lftp -e 'mirror --parallel=10 -R /var/Motiw /motiw/'$DIR'/var/Motiw' -u backup,$MOTIW 10.32.236.125
 lftp -e 'mirror --parallel=10 -R /etc /motiw/'$DIR'/etc' -u backup,$MOTIW 10.32.236.125
